75f813659c98ef599399130ae96d03120abc30d6
Mateen <zahidx297@yahoo.com>
2022-10-01 03:41:27 -0400

    let addBook create book dom element

--- javascript.js
+++ javascript.js
@@ -3,14 +3,24 @@ console.log('Hello, World!');
 const titleElement = document.querySelector('#title');
 const summaryElement = document.querySelector('#summary');
 const addBookButton = document.querySelector('#add-book')
+const bookHolder = document.querySelector('.container.bottom')
 
 addBookButton.addEventListener('click', addBook);
+console.log(titleElement.parentNode);
 
 function addBook() {
     let title = titleElement.value;
     let summary = summaryElement.value;
+    if (title === '')
+        return;
     titleElement.value = '';
     summaryElement.value = '';
+
+    let book = document.createElement('div');
+    book.classList.add('card');
+    book.textContent = title;
+    bookHolder.appendChild(book);
+
     console.dir(`title: ${title}, summary: ${summary}`);
 }
 
