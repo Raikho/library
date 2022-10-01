f3b3314d270d6545eaf7fca4b68cfd46103bfda5
Mateen <zahidx297@yahoo.com>
2022-10-01 03:20:37 -0400

    add basic addBook functionality

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 console.log('Hello, World!');
+
+const titleElement = document.querySelector('#title');
+const summaryElement = document.querySelector('#summary');
+const addBookButton = document.querySelector('#add-book')
+
+addBookButton.addEventListener('click', addBook);
+
+function addBook() {
+    let title = titleElement.value;
+    let summary = summaryElement.value;
+    titleElement.value = '';
+    summaryElement.value = '';
+    console.dir(`title: ${title}, summary: ${summary}`);
+}
+
