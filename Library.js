e9d0537732c7b2b9b074fcb15cd7804eb97e99d8
Mateen <zahidx297@yahoo.com>
2022-11-08 02:26:10 -0500

    add library constuctor functionality

--- Library.js
+++ Library.js
@@ -1,7 +1,34 @@
 export class Library {
     constructor() {
         this.array = [];
-        this.array.push(new Book());
+
+        this.addBookNode = document.getElementById('add-book');
+        this.titleNode = document.getElementById('title');
+        this.summaryNode = document.getElementById('summary');
+        this.pagesNode = document.getElementById('pages');
+        this.readNode = document.getElementById('read');
+        this.favoriteNode = document.getElementById('favorite');
+
+        this.addBookNode.addEventListener('click', this.addBook.bind(this));
+
+        console.log('constructing library...');
+    }
+    addBook() {
+        let title = this.titleNode.value || 'empty';
+        let summary = this.summaryNode.value || 'empty';
+        let pages = Number(this.pagesNode.value) || 0;
+        let isRead = this.readNode.checked;
+        let isFavorite = this.favoriteNode.checked;
+        this.resetInput();
+
+        console.log('adding book =>',{title, summary, pages, isRead, isFavorite});
+    }
+    resetInput() {
+        this.titleNode.value = '';
+        this.summaryNode.value = '';
+        this.pagesNode.value = '';
+        this.readNode.checked = false;
+        this.favoriteNode.checked = false;
     }
     print() {
         let out = 'Library: ';
--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 import {Library, Book} from "./Library.js"
 
-let book = new Book('The Title');
 let library = new Library();
 
-console.log(book.print());
 console.log(library.print());
